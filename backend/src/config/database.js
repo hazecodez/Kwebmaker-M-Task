faad9d9abefb4da17707c15f531427c83f7771c4
hazecodez <muhammedhashir298@gmail.com>
2024-09-20 22:40:45 +0530

    backend done

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,20 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: "mysql",
-//     port: process.env.DB_PORT,
-//   }
-// );
-
 const sequelize = new Sequelize(
-  "admin_panel_db",
-  "root",
-  "703475*haze",
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
